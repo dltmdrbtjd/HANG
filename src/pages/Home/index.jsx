b35cde3a0b7788ce91c1e1fd581abd1148072933
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-07-29 01:18:25 +0900

    이승규 <test> home에 제작한 components들test

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AreaSelectBox from '../../components/AreaSelectBox';
+import SearchBar from '../../components/SearchBar';
+import Modal from '../../components/Modal';
+import { Button } from '../../elements/index';
 
 const Home = () => {
-  return <AreaSelectBox />;
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const agreeModal = () => {
+    setModalOpen(false);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  return (
+    <>
+      <AreaSelectBox />
+      <SearchBar />
+      <Modal open={modalOpen} agree={agreeModal} close={closeModal} />
+      <Button width="100%" margin="50px 0 0 0" _onClick={openModal}>
+        신청하기 TEST
+      </Button>
+    </>
+  );
 };
 
 export default Home;
