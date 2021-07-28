db74d4f535635309c498b136a6d29ab2498c9dbd
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-07-29 01:15:45 +0900

    이승규 <style> Modal Style

---
+++ src/components/Modal/style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const ModalStyle = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  text-align: center;
+`;
+
+export default ModalStyle;
