4c7c454994b1b88c62643b6111fa593f7f78bbc3
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-07-28 23:11:46 +0900

    이승규 <error> rules수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,12 +10,15 @@ module.exports = {
     'plugin:prettier/recommended',
   ],
   rules: {
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/no-noninteractive-element-interactions': 0,
     'react/prop-types': 0,
     'no-undef': 0,
     'react/require-default-props': 0,
     'import/no-extraneous-dependencies': 0,
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'react/jsx-props-no-spreading': 'off',
+    'react/no-array-index-key': 0,
     'no-param-reassign': [
       'error',
       { props: true, ignorePropertyModificationsFor: ['draft'] },
