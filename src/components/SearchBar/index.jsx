463172c8f304359f22528c254b617b87ab4071f7
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-07-29 01:16:28 +0900

    이승규 <component> SearchBar Component

---
+++ src/components/SearchBar/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import SearchIcon from '@material-ui/icons/Search';
+import { Grid, Input } from '../../elements/index';
+
+const SearchBar = () => {
+  return (
+    <Grid margin="10px 0 0 0" position="relative">
+      <Input placeholder="가이디/트리비를 검색하세요." />
+      <Grid
+        width="30px"
+        height="30px"
+        position="absolute"
+        right="10px"
+        top="14px"
+        color="gray"
+      >
+        <SearchIcon />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default SearchBar;
