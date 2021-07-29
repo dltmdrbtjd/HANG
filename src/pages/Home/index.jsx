a5dc581605e762fb522e87499cae93e7a4c353c2
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-07-29 20:15:10 +0900

    props로 내부 정보변경 test

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,18 +9,30 @@ const Home = () => {
     <Grid overflow="auto">
       <PromiseCard>
         <Text fs="la" fw="bold">
-          아직 약속이 없어요!
+          다른 지역으로 떠나보는 건 어떠세요?
         </Text>
-        <Text fs="sm">여행 일정을 공유하고 가이딩 약속을 잡아 볼까요?</Text>
+        <Text fs="sm" fw="regular">
+          여행 일정을 공유하고 약속을 잡아 볼까요?
+        </Text>
       </PromiseCard>
       <SearchBar />
       <Grid margin="60px 0 0 0">
         <MainTitle fs="la">내 지역을 여행하고 싶은 여행자에요</MainTitle>
-        <SearchCard />
+        <SearchCard
+          username="새싹님"
+          age="20대"
+          gender="남자"
+          city="서울특별시 관악구"
+        />
       </Grid>
       <Grid margin="60px 0 0 0">
         <MainTitle fs="la">내 지역에 함께있는 길잡이에요</MainTitle>
-        <SearchCard />
+        <SearchCard
+          username="새싹몬"
+          age="20대"
+          gender="남자"
+          city="서울특별시 동작구"
+        />
       </Grid>
     </Grid>
   );
