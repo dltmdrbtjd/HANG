362fa44e376115e9aac55b7042d2f5fafd9be842
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-07-29 15:19:37 +0900

    이승규 <component> main 컴포넌트 제작중

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,33 +1,28 @@
-import React, { useState } from 'react';
-import AreaSelectBox from '../../components/AreaSelectBox';
+import React from 'react';
 import SearchBar from '../../components/SearchBar';
-import Modal from '../../components/Modal';
-import { Button } from '../../elements/index';
+import PromiseCard from './style';
+import SearchCard from '../../components/SearchCard';
+import { Grid, Text, MainTitle } from '../../elements/index';
 
 const Home = () => {
-  const [modalOpen, setModalOpen] = useState(false);
-
-  const openModal = () => {
-    setModalOpen(true);
-  };
-
-  const agreeModal = () => {
-    setModalOpen(false);
-  };
-
-  const closeModal = () => {
-    setModalOpen(false);
-  };
-
   return (
-    <>
-      <AreaSelectBox />
+    <Grid overflow="auto">
+      <PromiseCard>
+        <Text fs="la" fw="bold">
+          아직 약속이 없어요!
+        </Text>
+        <Text fs="sm">여행 일정을 공유하고 가이딩 약속을 잡아 볼까요?</Text>
+      </PromiseCard>
       <SearchBar />
-      <Modal open={modalOpen} agree={agreeModal} close={closeModal} />
-      <Button width="100%" margin="50px 0 0 0" _onClick={openModal}>
-        신청하기 TEST
-      </Button>
-    </>
+      <Grid margin="60px 0 0 0">
+        <MainTitle fs="la">내 지역을 여행하고 싶은 여행자에요</MainTitle>
+        <SearchCard />
+      </Grid>
+      <Grid margin="60px 0 0 0">
+        <MainTitle fs="la">내 지역에 함께있는 길잡이에요</MainTitle>
+        <SearchCard />
+      </Grid>
+    </Grid>
   );
 };
 
