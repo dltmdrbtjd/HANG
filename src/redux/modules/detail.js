6665adfae42337738fea77cb28483ea34ed4b6b6
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-08-06 23:03:23 +0900

    이승규 <redux> detail redux 불러오기,신청하기,되어주기

--- src/redux/modules/detail.js
+++ src/redux/modules/detail.js
@@ -3,18 +3,22 @@ import produce from 'immer';
 import apis from '../../shared/api';
 
 const LOAD = 'detail/LOAD';
-const ADDTRAVELE = 'detail/ADDTRAVELE';
-const ADDGUIDE = 'detail/ADDGUIDE';
+const MYTRAVELE_LOAD = 'detail/MYTRAVEL_LOAD';
+const SUCCESS = 'detail/SUCCESS';
 
 const DetailLoad = createAction(LOAD, (userInfo, tripInfo) => ({
   userInfo,
   tripInfo,
 }));
 
+const MyTravelLoad = createAction(MYTRAVELE_LOAD, myTrip => ({ myTrip }));
+const SuccessValue = createAction(SUCCESS, success => ({ success }));
+
 const initialState = {
   userInfo: {},
   tripInfo: [],
   myTripInfo: [],
+  success: false,
 };
 
 const DetailLoadDB = userPk => {
@@ -29,6 +33,47 @@ const DetailLoadDB = userPk => {
   };
 };
 
+const MyTripInfoDB = () => {
+  return dispatch => {
+    apis
+      .MyPromise()
+      .then(res => {
+        const data = res.data;
+        dispatch(MyTravelLoad(data));
+        dispatch(SuccessValue(false));
+      })
+      .catch(err => console.error(err));
+  };
+};
+
+const AddTravel = TripInfo => {
+  return (dispatch, getState, { history }) => {
+    apis
+      .GuideRequest(TripInfo)
+      .then(res => {
+        dispatch(SuccessValue(true));
+        history.goBack();
+      })
+      .catch(err =>
+        window.alert('선택한 여행일정은 해당유저에게 이미 신청하셨습니다.'),
+      );
+  };
+};
+
+const AddGuide = TripInfo => {
+  return dispatch => {
+    apis
+      .DoGuide({ tripId: TripInfo })
+      .then(res => {
+        dispatch(SuccessValue(true));
+      })
+      .catch(err => {
+        window.alert(err.response.data.errorMessage);
+        dispatch(SuccessValue(false));
+      });
+  };
+};
+
 export default handleActions(
   {
     [LOAD]: (state, action) =>
@@ -36,10 +81,24 @@ export default handleActions(
         draft.userInfo = action.payload.userInfo;
         draft.tripInfo = action.payload.tripInfo;
       }),
+    [MYTRAVELE_LOAD]: (state, action) =>
+      produce(state, draft => {
+        draft.myTripInfo = action.payload.myTrip;
+      }),
+    [SUCCESS]: (state, action) =>
+      produce(state, draft => {
+        draft.success = action.payload.success;
+      }),
   },
   initialState,
 );
 
-const DetailCreators = { DetailLoadDB };
+const DetailCreators = {
+  DetailLoadDB,
+  MyTripInfoDB,
+  AddTravel,
+  AddGuide,
+  SuccessValue,
+};
 
 export { DetailCreators };
