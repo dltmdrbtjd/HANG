0d93e081bc21b115392accdc741d69cc8440bdea
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-08-18 01:45:49 +0900

    이승규 <components> SelectBox 작업중

--- src/components/AreaSelectBox/index.tsx
+++ src/components/AreaSelectBox/index.tsx
@@ -1,1 +1,38 @@
-zz;
+import React from 'react';
+import { Grid, Ul } from 'src/elements';
+import CityArr from './cityArr';
+
+export interface Props {
+  toggle?: boolean;
+}
+
+const AreaSelectBox: React.FC<Props> = ({
+  toggle,
+  ...props
+}): React.ReactElement => {
+  const [currentCity, setCurrentCity] = React.useState<number>(0);
+  const [currentGu, setCurrentGu] = React.useState<number>(0);
+
+  const [cityName, setCityName] = React.useState<string>('');
+  const [guName, setGuName] = React.useState<string>('');
+
+  return (
+    <>
+      {toggle ? (
+        <Grid
+          width="100%"
+          radius="14px"
+          padding="12px"
+          bgColor="semiLightG"
+          isFlex
+          hoz="space-between"
+        >
+          <Ul>ㅎㅇ</Ul>
+          <Ul>ㅎㅇ</Ul>
+        </Grid>
+      ) : null}
+    </>
+  );
+};
+
+export default AreaSelectBox;
