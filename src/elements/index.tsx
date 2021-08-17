e7444a0a8338ccb7ce25aee72ff08e9cb0c89332
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-08-18 00:50:46 +0900

    이승규 <elem> blurbox elem

---
+++ src/elements/index.tsx
@@ -0,0 +1,10 @@
+import BlurBox from './BlurBox';
+import Input from './Input';
+import Label from './Label';
+import MainTitle from './MainTitle';
+import Strong from './Strong';
+import SubTitle from './SubTitle';
+import Text from './Text';
+import TextArea from './TextArea';
+
+export { BlurBox, Input, Label, MainTitle, Strong, SubTitle, Text, TextArea };
