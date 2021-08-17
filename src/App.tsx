bba1aeb70e9a6321cfd0398641f0321c0bc7b87c
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-08-17 22:57:21 +0900

    이승규 <style> button elem test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import Input from './elements/Input';
 
 const App = () => {
-  return <div className="App">웰컴투 타입스크립트</div>;
+  return (
+    <>
+      <div className="App">웰컴투 타입스크립트</div>
+      <Input
+        placeholder="여행자/길잡이를 검색하세요"
+        radius="40px"
+        padding="11px 45px 11px 23px"
+        fs="14px"
+      />
+    </>
+  );
 };
 
 export default App;
