2e630423476209291d950e16d3b9ad773bbee97d
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-08-17 21:20:40 +0900

    이승규 <pwa> pwa serviceWorker 기본 설정 및 세팅

---
+++ src/service-worker.ts
@@ -0,0 +1,49 @@
+/// <reference lib="webworker" />
+import { clientsClaim } from 'workbox-core';
+import { ExpirationPlugin } from 'workbox-expiration';
+import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
+import { registerRoute } from 'workbox-routing';
+import { StaleWhileRevalidate } from 'workbox-strategies';
+
+declare const self: ServiceWorkerGlobalScope;
+
+clientsClaim();
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$');
+registerRoute(
+  ({ request, url }: { request: Request; url: URL }) => {
+    if (request.mode !== 'navigate') {
+      return false;
+    }
+
+    if (url.pathname.startsWith('/_')) {
+      return false;
+    }
+
+    if (url.pathname.match(fileExtensionRegexp)) {
+      return false;
+    }
+
+    return true;
+  },
+  createHandlerBoundToURL(`${process.env.PUBLIC_URL  }/index.html`)
+);
+
+registerRoute(
+  ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'),
+  new StaleWhileRevalidate({
+    cacheName: 'images',
+    plugins: [
+      new ExpirationPlugin({ maxEntries: 50 }),
+    ],
+  })
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
