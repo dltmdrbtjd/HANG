fd048d5b1b0c357f565cd245380b2c0eb6ae5344
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-08-18 00:50:28 +0900

    이승규 <elem> label elem

---
+++ src/elements/Label/style.ts
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+// mixin
+import { borderBox, textProps, floatBox } from 'src/styles/Mixin';
+
+export interface Prop {
+  fw?: string;
+  fs?: string;
+  color?: string;
+  lh?: string;
+  textAlign?: string;
+  margin?: string;
+  width?: string;
+  height?: string;
+  paddig?: string;
+  position?: string;
+  top?: string;
+  right?: string;
+  bottom?: string;
+  left?: string;
+  z?: string;
+  addstyle?: any;
+}
+
+const LabelStyle = styled.label<Prop>`
+  ${({fs, fw, color, lh, textAlign}) => textProps(fs, fw, color, lh, textAlign)};
+  margin: ${({margin}) => margin};
+  width: ${({width}) => width};
+  height: ${({height}) => height};
+  ${({padding}) => borderBox(padding)};
+  ${({position, top, right, bottom, left, z}) => floatBox(position, top, right, bottom, left, z)};
+  ${({addstyle}) => addstyle};
+`;
+
+export default LabelStyle;
