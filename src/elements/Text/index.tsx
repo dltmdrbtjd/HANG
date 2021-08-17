4a9ed37b2f330d865992c9abfd07f54a541da036
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-08-18 00:28:05 +0900

    이승규 <elem> p elem

---
+++ src/elements/Text/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import TextStyle from './style';
+
+export interface Props {
+  children: string;
+  width?: string;
+  height?: string;
+  margin?: string;
+  padding?: string;
+  fs?: string;
+  fw?: string;
+  color?: string;
+  lh?: string;
+  textAlign?: string;
+  ls?: string;
+  wb?: string;
+  ws?: string;
+  addstyle?: any;
+}
+
+const Text: React.FC<Props> = ({ children, ...props }): React.ReactElement => {
+  return <TextStyle {...props}>{children}</TextStyle>;
+};
+
+export default Text;
