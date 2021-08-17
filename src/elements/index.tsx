5a3f6548927001d88a04e64148f8faae9fad560f
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-08-18 00:57:49 +0900

    이승규 <elem> elem index 통합 파일

--- src/elements/index.tsx
+++ src/elements/index.tsx
@@ -6,5 +6,34 @@ import Strong from './Strong';
 import SubTitle from './SubTitle';
 import Text from './Text';
 import TextArea from './TextArea';
+import Button from './Button';
+import Container from './Container';
+import Grid from './Grid';
+import Hr from './Hr';
+import Image from './Image';
+import InputRadio from './InputRadio';
+import List from './List';
+import Logo from './Logo';
+import Span from './Span';
+import Ul from './Ul';
 
-export { BlurBox, Input, Label, MainTitle, Strong, SubTitle, Text, TextArea };
+export {
+  BlurBox,
+  Input,
+  Label,
+  MainTitle,
+  Strong,
+  SubTitle,
+  Text,
+  TextArea,
+  Button,
+  Container,
+  Grid,
+  Hr,
+  Image,
+  InputRadio,
+  List,
+  Logo,
+  Span,
+  Ul,
+};
