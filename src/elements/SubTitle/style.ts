53f54167810d6a8d52a25ecb13a15ba45bc5b9ab
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-08-18 00:27:50 +0900

    이승규 <elem> h3 elem

---
+++ src/elements/SubTitle/style.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+// mixin
+import { borderBox, textProps } from 'src/styles/Mixin';
+
+export interface Prop {
+  width?: string;
+  height?: string;
+  margin?: string;
+  padding?: string;
+  fs?: string;
+  fw?: string;
+  color?: string;
+  lh?: string;
+  textAlign?: string;
+  addstyle?: any;
+}
+
+const SubTitleStyle = styled.h3<Prop>`
+  width: ${({width}) => width};
+  height: ${({height}) => height};
+  margin: ${({margin}) => margin};
+  ${({padding}) => borderBox(padding)};
+  ${({fs,fw,color,lh,textAlign}) => textProps(fs,fw,color,lh,textAlign)};
+  ${({addstyle}) => addstyle};
+`;
+
+export default SubTitleStyle;
