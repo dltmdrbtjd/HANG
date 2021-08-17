e049dac7c82cd9c9efee774f047104d1288e925b
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-08-18 00:28:24 +0900

    이승규 <elem> textarea elem

---
+++ src/elements/TextArea/style.ts
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+// mixin
+import { borderBox } from 'src/styles/Mixin';
+
+export interface Prop {
+  radius?: string;
+  width?: string;
+  height?: string;
+  bgColor?: string;
+  color?: string;
+  fs?: string;
+  padding?: string;
+  border?: string;
+  addstyle?: string;
+  theme: {
+    [propName: string]: any;
+  }
+}
+
+const TextAreaStyle = styled.textarea`
+  border-radius: ${({radius}) => radius};
+  width: ${({width}) => width};
+  height: ${({height}) => height};
+  background-color: ${({theme, bgColor}) => theme.color[bgColor]};
+  color: ${({theme, color}) => theme.color[color]};
+  font-size: ${({theme, fs}) => theme.fontSize[fs]};
+  resize: none;
+  ${({padding}) => borderBox(padding)};
+  border: ${({border}) => border};
+
+  &:focus {
+    outline: none;
+  }
+
+  ${({addstyle}) => addstyle};
+`;
+
+export default TextAreaStyle;
