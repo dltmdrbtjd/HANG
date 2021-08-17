ffdadd3759ee3d34d44747d5b86ba00b2c098c89
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-08-18 02:28:46 +0900

    이승규 <components> AreaSelectBox Components

--- src/components/AreaSelectBox/index.tsx
+++ src/components/AreaSelectBox/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Grid, Ul } from 'src/elements';
+import { Grid, List, Ul } from 'src/elements';
 import CityArr from './cityArr';
+import CityTab from './style';
 
 export interface Props {
   toggle?: boolean;
@@ -11,10 +12,31 @@ const AreaSelectBox: React.FC<Props> = ({
   ...props
 }): React.ReactElement => {
   const [currentCity, setCurrentCity] = React.useState<number>(0);
-  const [currentGu, setCurrentGu] = React.useState<number>(0);
+  const [currentGu, setCurrntGu] = React.useState<number>(0);
 
   const [cityName, setCityName] = React.useState<string>('');
   const [guName, setGuName] = React.useState<string>('');
+
+  const SelectCityHandler = (idx: number, city: string) => {
+    setCurrentCity(idx);
+    setCurrntGu(0);
+    setCityName(city);
+    if (idx === 0) {
+      setGuName('강남구');
+    } else if (idx === 1) {
+      setGuName('강서구');
+    } else {
+      setGuName('구좌읍');
+    }
+  };
+
+  const SelectCurrentGu = (idx: number, gu: string) => {
+    setCurrntGu(idx);
+    setGuName(gu);
+    if (!cityName) {
+      setCityName('서울특별시');
+    }
+  };
 
   return (
     <>
@@ -23,12 +45,66 @@ const AreaSelectBox: React.FC<Props> = ({
           width="100%"
           radius="14px"
           padding="12px"
-          bgColor="semiLightG"
+          bgColor="lightGray"
           isFlex
           hoz="space-between"
         >
-          <Ul>ㅎㅇ</Ul>
-          <Ul>ㅎㅇ</Ul>
+          <Ul
+            bgColor="bgColor"
+            fw="bold"
+            isFlex
+            ver="align-items"
+            textAlign="left"
+            radius="14px"
+            padding="10px 15px 10px 10px"
+            width="49%"
+            height="185px"
+            overflow="auto"
+            addstyle={CityTab}
+          >
+            {CityArr.map((ele: any, idx: number) => {
+              return (
+                <List
+                  key={idx}
+                  className={
+                    currentCity === idx ? 'submenu focused' : 'submenu'
+                  }
+                  _onClick={() => {
+                    SelectCityHandler(idx, ele.city);
+                  }}
+                >
+                  {ele.city}
+                </List>
+              );
+            })}
+          </Ul>
+          <Ul
+            bgColor="bgColor"
+            fw="bold"
+            isFlex
+            ver="align-items"
+            textAlign="left"
+            radius="14px"
+            padding="10px 15px 10px 10px"
+            width="49%"
+            height="185px"
+            overflow="auto"
+            addstyle={CityTab}
+          >
+            {CityArr[currentCity].gu.map((ele: any, idx: number) => {
+              return (
+                <List
+                  key={idx}
+                  className={currentGu === idx ? 'submenu focused' : 'submenu'}
+                  _onClick={() => {
+                    SelectCurrentGu(idx, ele);
+                  }}
+                >
+                  {ele}
+                </List>
+              );
+            })}
+          </Ul>
         </Grid>
       ) : null}
     </>
