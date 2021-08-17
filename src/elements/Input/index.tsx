7782fe04d768439afd48396beba94d1a577110e1
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-08-17 22:58:09 +0900

    이승규 <elem> Input style 완성

---
+++ src/elements/Input/index.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import InputStyle from './style';
+
+export interface Props {
+  width?: string;
+  height?: string;
+  bgColor?: string;
+  padding?: string;
+  radius?: string;
+  border?: string;
+  fs?: string;
+  shadow?: string;
+  id?: string;
+  type?: string;
+  name?: string;
+  value?: string;
+  accept?: string;
+  placeholder?: string;
+  _onChange?: any;
+  _onKeyPress?: any;
+}
+
+const Input: React.FC<Props> = ({
+  id,
+  type,
+  name,
+  value,
+  accept,
+  placeholder,
+  _onChange,
+  _onKeyPress,
+  ...props
+}): React.ReactElement => {
+  return (
+    <InputStyle
+      id={id}
+      value={value}
+      placeholder={placeholder}
+      type={type}
+      accept={accept}
+      onChange={_onChange}
+      onkeyPress={_onKeyPress}
+      name={name}
+      {...props}
+    />
+  );
+};
+
+Input.defaultProps = {
+  _onChange: () => {},
+  _onKeyPress: () => {},
+  type: 'text',
+  placeholder: '',
+  width: '100%',
+  height: '48px',
+  bgColor: 'white',
+  padding: '0 12px',
+  radius: '14px',
+  border: '1px solid #E7E7E7',
+};
+
+export default Input;
