205f0e07038326885d9b77e4f37226d9abddc4e1
unknown <hh4518@gmail.com>
2021-08-18 23:13:46 +0900

    홍성훈: <router> 기본 라우터 설정 완료

---
+++ src/route/Path.ts
@@ -0,0 +1,36 @@
+const pathURI = {
+  home: '/',
+  onboarding: '/onboarding',
+  logIn: '/login',
+  signUp: '/signup',
+  forgotPwd: '/signup/forgot_pwd',
+  welcome: '/signup/welcome',
+  search: '/search',
+  detail: '/detail',
+  request: '/detail/request',
+  favorite: '/favorite',
+  mypage: '/mypage',
+  promise: '/mypage/promise',
+  block: '/mypage/block',
+  noti: '/noti',
+  chat: '/chat',
+  chatRoom: '/chat/room',
+};
+
+const HeaderIncluded: string[] = [
+  '/',
+  '/search',
+  '/detail',
+  '/detail/request',
+  '/favorite',
+  '/mypage',
+  '/mypage/block',
+  '/mypage/promise',
+  '/noti',
+  '/chat',
+  '/chat/room',
+  '/mypage/modify',
+  '/mypage/create_trip',
+];
+
+export { pathURI, HeaderIncluded };
