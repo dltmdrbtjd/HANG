5054f965c3bba45ac9ee47a3e25caace9da661f0
unknown <hh4518@gmail.com>
2021-08-18 10:19:03 +0900

    홍성훈: <components> DropDown components 제작 완료

---
+++ src/components/DropDown/index.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+// material
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+// style
+import { outlineBox } from 'src/styles/Mixin';
+// elements
+import { Button, Ul, List } from '../../elements';
+
+export interface Props {
+  icon: React.ReactElement;
+  contents: string[];
+  methods: any[];
+  top: string;
+}
+
+const DropDown: React.FC<Props> = ({ icon, contents, methods, top }) => {
+  const [open, setOpen] = React.useState<boolean>(false);
+  const anchorRef = React.useRef<HTMLButtonElement>(null);
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (e) => {
+    if (anchorRef.current && anchorRef.current.contains(e.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <Button form="text" padding="0" ref={anchorRef} _onClick={handleToggle}>
+        {icon}
+      </Button>
+
+      {open ? (
+        <ClickAwayListener onClickAway={handleClose}>
+          <Ul
+            width="auto"
+            bgColor="white"
+            border="1px solid #E7E7E7"
+            position="absolute"
+            top={top}
+            right="0"
+            z="9"
+            shadow="0px 2px 3px rgba(196, 196, 196, 0.25)"
+          >
+            {contents.map((content: string, idx: number) => {
+              const isLast = idx === contents.length - 1;
+
+              return (
+                <List
+                  key={(idx * Date.now() + Math.random()).toString(36)}
+                  padding="16px 30px"
+                  _onClick={() => methods[idx]()}
+                  addstyle={isLast || outlineBox('1px solid #E7E7E7', 'bottom')}
+                >
+                  {content}
+                </List>
+              );
+            })}
+          </Ul>
+        </ClickAwayListener>
+      ) : null}
+    </>
+  );
+};
+
+export default DropDown;
