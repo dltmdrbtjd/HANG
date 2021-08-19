4c415c7a8d154390a5cf5e8e3100f1e9fec63a2d
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-08-20 03:32:30 +0900

    이승규 <page> 하단에 navigation추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import { ConnectedRouter } from 'connected-react-router';
 import Route from './Route/Route';
 // history
 import { history } from './redux/configureStore';
+import Navigation from './components/Navigation';
 
 const App = (): React.ReactElement => {
   return (
     <ConnectedRouter history={history}>
       <Route />
+      <Navigation />
     </ConnectedRouter>
   );
 };
