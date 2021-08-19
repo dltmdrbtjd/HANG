c4165676f3dcb0effc819fabc9c7d54bf47d169b
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-08-20 03:39:02 +0900

    이승규 <page> 알람페이지 기능,뷰 완료

---
+++ src/pages/Noti/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from 'src/redux/configureStore';
+import { AlarmCreators } from 'src/redux/modules/AlarmModule/alarm';
+import { Grid, Button, Container } from '../../elements';
+import maxWidth from './style';
+import AlaremCard from './AlarmCard';
+
+const Noti = () => {
+  const dispatch = useDispatch();
+  const list: any = useSelector<RootState>((state) => state.alarm.list);
+
+  const AlarmDeleteBtn = () => {
+    dispatch(AlarmCreators.fetchDeleteAlarm());
+  };
+
+  React.useEffect(() => {
+    dispatch(AlarmCreators.fetchAlarmLoad());
+  }, []);
+  return (
+    <Container>
+      <Grid margin="-24px 0 80px">
+        {list
+          ? list.map((item, idx) => {
+              return <AlaremCard userInfo={item} key={idx} />;
+            })
+          : ''}
+
+        <Grid
+          width="90%"
+          position="fixed"
+          bottom="110px"
+          left="50%"
+          translate="-50%, 0"
+          // tab="max-width: 768px"
+          addstyle={maxWidth}
+        >
+          <Button width="100%" fs="la" _onClick={AlarmDeleteBtn}>
+            전체삭제
+          </Button>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default Noti;
