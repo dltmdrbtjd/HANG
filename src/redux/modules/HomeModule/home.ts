06f4cf3f3c503b7ad35d36f9a0c2a031c96415e6
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-08-19 16:19:11 +0900

    이승규 <redux> home module 완성

---
+++ src/redux/modules/HomeModule/home.ts
@@ -0,0 +1,79 @@
+import { createSlice, PayloadAction, createAsyncThunk } from '@reduxjs/toolkit';
+// apis
+import apis from '../../../shared/api';
+// types
+import { homeType, home, like } from './type';
+
+export const initialState: homeType = {
+  HomeData: {
+    confirmed: null,
+    guide: [],
+    traveler: [],
+  },
+  loading: false,
+}
+
+const fetchHomeLoad = createAsyncThunk('home/HOME_LOAD', async () => {
+  try {
+    const response = (await apis.MainLoad()).data;
+    return response;
+  } catch (err) {
+    return console.log(err);
+  };
+})
+
+const homeSlice = createSlice({
+  name: 'home',
+  initialState,
+  reducers: {
+    LikeUpdate: (state, action: PayloadAction<like>) => {
+      let TraveleDoubleCheck;
+      let GuideDoubleCheck;
+
+      if(state.HomeData.guide.length > 0){
+        TraveleDoubleCheck = state.HomeData.traveler.findIndex(
+          i => i.userPk === state.HomeData.guide[action.payload.idx].userPk,
+        );
+      }
+      if(state.HomeData.traveler.length > 0) {
+        GuideDoubleCheck = state.HomeData.guide.findIndex(
+          i => i.userPk === state.HomeData.traveler[action.payload.idx].userPk,
+        );
+      }
+
+      if(action.payload.category === 'guide'){
+        state.HomeData.guide[action.payload.idx].like = action.payload.like;
+        if(TraveleDoubleCheck !== -1) {
+          state.HomeData.traveler[TraveleDoubleCheck].like = action.payload.like;
+        }
+      } else if (action.payload.category === 'traveler'){
+        state.HomeData.traveler[action.payload.idx].like = action.payload.like;
+        if(GuideDoubleCheck !== -1) {
+          state.HomeData.guide[GuideDoubleCheck].like = action.payload.like;
+        }
+      }
+    }
+  },
+  extraReducers: {
+    [fetchHomeLoad.pending.type]: (state) => {
+      state.loading = true;
+    },
+    [fetchHomeLoad.fulfilled.type]: (state,action: PayloadAction<home>) => {
+      state.loading = false;
+      state.HomeData = action.payload;
+    },
+    [fetchHomeLoad.rejected.type]: (state) => {
+      state.loading = false;
+      state.HomeData = initialState.HomeData;
+    }
+  }
+});
+
+const HomeCreators = {
+  fetchHomeLoad,
+}
+
+export { HomeCreators }
+const { reducer,actions } = homeSlice;
+export const { LikeUpdate } = actions;
+export default reducer;
