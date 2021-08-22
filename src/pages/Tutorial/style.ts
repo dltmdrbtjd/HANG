8fda0434d7bf6e7d329103260a2b85e50906f41a
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-08-23 00:34:39 +0900

    이승규 <page> 디자인 수정 및 반응형

--- src/pages/Tutorial/style.ts
+++ src/pages/Tutorial/style.ts
@@ -1,11 +1,21 @@
 import { css } from 'styled-components';
 
 const ButtonStyle = css`
+  max-width: 550px;
   position: absolute;
-  bottom: 100px;
+  bottom: 30px;
   left: 50%;
   transform: translateX(-50%);
   z-index: 10;
 `;
 
-export default ButtonStyle;
+const boxWidth = css`
+  max-width: 768px;
+`;
+
+const ImageStyle = css`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+export {ButtonStyle, ImageStyle, boxWidth};
