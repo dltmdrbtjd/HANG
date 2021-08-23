4372a608918a15942ed98945c3c160d5ea216560
unknown <hh4518@gmail.com>
2021-08-23 11:02:15 +0900

    홍성훈: <style> 탭 반응형 작업 완료

--- src/pages/Noti/style.ts
+++ src/pages/Noti/style.ts
@@ -1,11 +1,7 @@
 import { css } from 'styled-components';
-// media
-import { setMediaLimitBoxSize } from '../../styles/Media';
 
 const maxWidth = css`
   max-width: 600px;
-
-  ${setMediaLimitBoxSize('768px')}
 `;
 
 export default maxWidth;
