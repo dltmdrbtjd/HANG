d580bcb19745c0871c8dc3787e03e6b2e8ce4e6b
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-08-25 21:34:33 +0900

    이승규 <page> 목록이 없을시에 대체이미지 추가

---
+++ src/pages/Favorite/style.ts
@@ -0,0 +1,12 @@
+import {css} from 'styled-components';
+
+const NotFoundImage = css`
+  position: absolute;
+  max-width: 350px;
+  width: 70%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+`;
+
+export default NotFoundImage;
