ea1e06f761e97e13bdbf116f21991714cce4439d
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-08-29 05:11:18 +0900

    이승규 <api> baseURL 변경

--- src/util/socket.ts
+++ src/util/socket.ts
@@ -1,4 +1,4 @@
 import io from 'socket.io-client';
 
-const EndPoint = 'https://soujinko.shop';
-export const socket = io(EndPoint);
+const EndPoint = 'https://ruzan.shop';
+export const socket = io(EndPoint, {transports:['websocket']});
